313064cea744ba8dcb51c8c2252cb266d3d998f9
Benjamin <benjaminprogrammering@gmail.com>
2021-05-21 22:16:47 +0200

    Added bullets and more!
    Signed-off-by: Benjamin <benjaminprogrammering@gmail.com>

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
 /*
-  Copyright 2020 Benjamin Ojanne - All rights reserved
- */
+ *
+ * Copyright 2020 Benjamin Ojanne - All rights reserved
+ *
+*/
 use std::ptr::null;
 use std::time::{Duration, Instant};
 
@@ -8,24 +10,27 @@ use cgmath::mint::Vector2;
 use cgmath::num_traits::Pow;
 use ggez::{Context, ContextBuilder, GameResult, graphics};
 use ggez::event::{self, EventHandler, KeyCode, KeyMods};
-use ggez::graphics::{Color, Image, mint, Rect};
+use ggez::graphics::{Color, draw, Drawable, DrawParam, Image, mint, Rect};
+use ggez::graphics::spritebatch::{SpriteBatch, SpriteIdx};
 use ggez::input::mouse::MouseButton;
 use rand::Rng;
 
-const MAX_AIR_SPEED: f32 = 15.0;
-const MIN_AIR_SPEED: f32 = 0.0;
+const AIR_SPEED: f32 = 7.0;
 
-//const GRAVITY: i16 = 3;
+static mut RIGHT_KEY_PRESSED: bool = false;
+static mut LEFT_KEY_PRESSED: bool = false;
+static mut DOWN_KEY_PRESSED: bool = false;
+static mut UP_KEY_PRESSED: bool = false;
 
-//Screen size
 const GRID_SIZE: (i16, i16) = (35, 20);
 const GRID_CELL_SIZE: (i16, i16) = (34, 34);
+
 const SCREEN_SIZE: (f32, f32) = (
     GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
     GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
 );
 
-const UPDATES_PER_SECOND: f32 = 50.0;
+const UPDATES_PER_SECOND: f32 = 120.0;
 
 // And we get the milliseconds of delay that this update rate corresponds to.
 const MILLIS_PER_UPDATE: u64 = (1.0 / UPDATES_PER_SECOND * 1000.0) as u64;
@@ -33,12 +38,15 @@ const MILLIS_PER_UPDATE: u64 = (1.0 / UPDATES_PER_SECOND * 1000.0) as u64;
 /* Start Function */
 pub fn start() {
     let (mut ctx, event_loop) = ContextBuilder::new("my_first_game", "Benjamin Ojanne")
-        .window_setup(ggez::conf::WindowSetup::default().title("Airplane Game - Version: 1.0.0").icon("/plane.png"))
+        .window_setup(
+            ggez::conf::WindowSetup::default()
+                .title("Airplane Game - Version: 1.0.0")
+                .icon("/plane.png"),
+        )
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .build()
         .expect("Failed to create ggez context!!");
 
-    //Create an instance
     let game_state = MyGameState::new(&mut ctx);
 
     //Run the game!
@@ -53,13 +61,13 @@ pub fn start() {
 struct MyGameState {
     plane: Plane,
     background: Background,
+    bullets: Vec<Bullet>,
     game_over: bool,
     last_update: Instant,
 }
 
 impl MyGameState {
     pub fn new(ctx: &mut Context) -> Self {
-
         // Load / create resources.
 
         let player_pos = (GRID_SIZE.0 / 2, GRID_SIZE.1 / 2).into();
@@ -72,9 +80,24 @@ impl MyGameState {
 
         let img_width = background_img.width();
 
+        let mut bullets: Vec<Bullet> = Vec::new();
+
+        //10 - times
+        for _ in 1..11 {
+            let mut shot_img = graphics::Image::new(ctx, "/fire_ball_1.png").unwrap();
+            bullets.push(Bullet::new(shot_img, false));
+        }
+
+        //10 - times
+        for _ in 1..11 {
+            let mut shot_img = graphics::Image::new(ctx, "/torpedo.png").unwrap();
+            bullets.push(Bullet::new(shot_img, true));
+        }
+
         MyGameState {
             plane: Plane::new(player_pos, image),
             background: Background::new(background_img, img_width),
+            bullets,
             game_over: false,
             last_update: Instant::now(),
         }
@@ -100,9 +123,9 @@ impl Background {
         }
     }
 
-    fn update(&mut self, speed: f32) {
-        self.x = self.x - 1.0;
-        self.x2 = self.x2 - 1.0;
+    fn update(&mut self) {
+        self.x = self.x - 3.0;
+        self.x2 = self.x2 - 3.0;
         if self.x2 < 0.0 {
             self.x2 = SCREEN_SIZE.0;
             self.x = 0.0;
@@ -112,28 +135,20 @@ impl Background {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         let mut params = graphics::DrawParam::new();
 
-        //  let dif = (SCREEN_SIZE.0 / (self.image.width() as f32)) * 1.2 - (SCREEN_SIZE.0 / (self.image.width() as f32));
-
         params = params.scale(mint::Vector2 {
             x: SCREEN_SIZE.0 / self.image.width() as f32,
             y: SCREEN_SIZE.1 / self.image.height() as f32,
         });
 
-        params = params.dest(mint::Vector2 {
-            x: self.x,
-            y: 0.0,
-        });
+        params = params.dest(mint::Vector2 { x: self.x, y: 0.0 });
 
         graphics::draw(ctx, &self.image, params)?;
 
-        params = params.dest(mint::Vector2 {
-            x: self.x2,
-            y: 0.0,
-        });
+        params = params.dest(mint::Vector2 { x: self.x2, y: 0.0 });
 
         graphics::draw(ctx, &self.image, params)?;
 
-        //Drop memory!
+        //Garbage collection
         std::mem::drop(params);
 
         Ok(())
@@ -143,14 +158,12 @@ impl Background {
 struct Plane {
     position: MapPosition,
     image: graphics::Image,
-    accelerate: bool,
-    flip: bool,
-    rotate_right: bool,
-    rotate_left: bool,
+    boost: bool,
+    pos: nalgebra::Point2<f32>,
     x: f32,
     y: f32,
+    angle: f32,
     speed: f32,
-    rotation: f32,
 }
 
 impl Plane {
@@ -158,76 +171,92 @@ impl Plane {
         Plane {
             position,
             image,
-            accelerate: false,
-            flip: false,
-            rotate_right: false,
-            rotate_left: false,
+            boost: false,
+            pos: nalgebra::Point2::new(0.0, 0.0),
             x: 400.0,
             y: 400.0,
+            angle: 0.0,
             speed: 0.0,
-            rotation: 0.0,
         }
     }
 
-    pub fn update(&mut self) {
-        if self.accelerate && self.speed < MAX_AIR_SPEED {
-            self.speed += 2.0;
-        } else if !self.accelerate && self.speed > 0.0 {
-            self.speed -= 2.0;
-        }
-        if self.rotate_right {
-            self.rotation += 0.1;
-        } else if self.rotate_left {
-            self.rotation -= 0.1;
+    pub unsafe fn update(&mut self) {
+        if RIGHT_KEY_PRESSED && self.speed < AIR_SPEED {
+            self.speed = AIR_SPEED;
+        } else if LEFT_KEY_PRESSED && self.speed > -AIR_SPEED {
+            self.speed = -AIR_SPEED;
         }
 
-        self.x = self.x + self.speed;
+        if UP_KEY_PRESSED {
+            self.y -= AIR_SPEED / 2.0;
+        }
+
+        if DOWN_KEY_PRESSED {
+            self.y += AIR_SPEED / 2.0;
+        }
+
+        if RIGHT_KEY_PRESSED && !LEFT_KEY_PRESSED {
+            self.x += self.speed;
+        } else if LEFT_KEY_PRESSED && !RIGHT_KEY_PRESSED {
+            self.x += self.speed;
+        }
 
         if self.x > SCREEN_SIZE.0 + (self.image.width() as f32) {
             self.x = 0.0;
+        } else if self.x < -(self.image.width() as f32) {
+            self.x = SCREEN_SIZE.0;
         }
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        /* if self.move_right {
-             if (self.position.x + &PLAYER_SPEED + self.width) <= SCREEN_SIZE.0 as i16 {
-                 self.position.x += PLAYER_SPEED;
-             }
-         }
-
-         if self.move_left {
-             if (self.position.x - &PLAYER_SPEED) >= 0 {
-                 self.position.x -= PLAYER_SPEED;
-             }
-         }*/
-
-
-        // And then we do the same for the head, instead making it fully red to distinguish it.
-        /*let rectangle = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            Rect::new(self.position.x as f32, self.position.y as f32, self.width as f32, self.height as f32),
-            [1.0, 0.5, 0.0, 1.0].into(),
-        )?;
-
-        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 }, ))?;*/
-
         let mut params = graphics::DrawParam::new();
 
-        params = params.scale(mint::Vector2 { x: if self.flip { -0.15 } else { 0.15 }, y: 0.15 });
+        params = params.scale(mint::Vector2 { x: 0.125, y: 0.125 });
 
         params = params.dest(mint::Vector2 {
             x: self.x,
             y: self.y,
         });
 
-        params = params.rotation(self.rotation);
+        params = params.rotation(self.angle);
 
         params = params.offset(mint::Point2 { x: 0.5, y: 0.5 });
 
         graphics::draw(ctx, &self.image, params)?;
 
+        std::mem::drop(params);
+
         Ok(())
+    }
+}
+
+struct Bullet {
+    img: Image,
+    x: f32,
+    y: f32,
+    active: bool,
+    torpedo: bool,
+}
+
+impl Bullet {
+    fn new(img: Image, torpedo: bool) -> Self {
+        Bullet {
+            img,
+            x: 0.0,
+            y: 0.0,
+            active: false,
+            torpedo,
+        }
+    }
+}
+
+struct Explosion {
+    sprite_batch: SpriteBatch,
+}
+
+impl Explosion {
+    fn new(sprite_batch: SpriteBatch) -> Self {
+        Explosion { sprite_batch }
     }
 }
 
@@ -238,10 +267,7 @@ struct MapPosition {
 
 impl MapPosition {
     pub fn new(x: i16, y: i16) -> Self {
-        MapPosition {
-            x,
-            y,
-        }
+        MapPosition { x, y }
     }
 
     /*fn get_random_pos() -> Self {
@@ -261,7 +287,8 @@ impl From<MapPosition> for graphics::Rect {
             pos.x as i32 * GRID_CELL_SIZE.0 as i32,
             pos.y as i32 * GRID_CELL_SIZE.1 as i32,
             GRID_CELL_SIZE.0 as i32,
-            GRID_CELL_SIZE.1 as i32, )
+            GRID_CELL_SIZE.1 as i32,
+        )
     }
 }
 
@@ -271,8 +298,10 @@ impl EventHandler for MyGameState {
             return Ok(());
         }
         if !self.game_over {
-            self.plane.update();
-            self.background.update(self.plane.speed);
+            unsafe {
+                self.plane.update();
+            }
+            self.background.update();
         }
         //Set last_update to be now
         self.last_update = Instant::now();
@@ -281,10 +310,40 @@ impl EventHandler for MyGameState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, Color::BLACK); //Cyan color!
+
         //Draw background first!
         self.background.draw(ctx)?;
+
         //draw player
         self.plane.draw(ctx)?;
+
+        //draw bullets
+        for bullet in self.bullets.iter_mut() {
+            if bullet.active {
+                bullet.x = bullet.x + 15.0;
+
+                if bullet.x > SCREEN_SIZE.0 {
+                    bullet.active = false;
+                }
+
+                let mut params: DrawParam = DrawParam::new();
+
+                params = params.dest(mint::Vector2 {
+                    x: bullet.x,
+                    y: bullet.y,
+                });
+
+                params = params.offset(mint::Point2 { x: 0.0, y: 0.5 });
+
+                params = params.scale(mint::Vector2 {
+                    x: if bullet.torpedo { 0.25 } else { 0.5 },
+                    y: if bullet.torpedo { 0.25 } else { 0.5 },
+                });
+
+                graphics::draw(ctx, &bullet.img, params)?;
+            }
+        }
+
         //Display new frame!
         graphics::present(ctx)?;
         //Yield the current thread until the next update
@@ -293,67 +352,59 @@ impl EventHandler for MyGameState {
         Ok(())
     }
 
-    /*fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) {
-        if _button == MouseButton::Left {
-            self.plane.accelerate = true;
-        }
-    }
-
-    fn mouse_button_up_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) {
-        if _button == MouseButton::Left {
-            self.plane.accelerate = false;
-        }
-    }*/
-
-    /*fn mouse_motion_event(&mut self, _ctx: &mut Context, _x: f32, _y: f32, _dx: f32, _dy: f32) {
-        let mut angle: f32 = 0.0;
-        if self.plane.y < _y && self.plane.x < _x {
-            let dif_x = f32::abs(self.plane.x - _x);
-            let dif_y = f32::abs(self.plane.y - _y);
-            let c = f32::sqrt((dif_x.pow(2) + dif_y.pow(2)));
-            angle = f32::asin(f32::sin(90.0) * dif_y / c);
-        } else if self.plane.y > _y && self.plane.x < _x {
-            let dif_x = self.plane.x - _x;
-            let dif_y = self.plane.y - _y;
-            let c = f32::sqrt((dif_x.pow(2) + dif_y.pow(2)));
-            angle = -f32::asin(f32::sin(90.0) * dif_y / c);
-        } else if self.plane.y > _y && _x < self.plane.x {
-            let dif_x = f32::abs(self.plane.x - _x);
-            let dif_y = f32::abs(self.plane.y - _y);
-            let c = f32::sqrt((dif_x.pow(2) + dif_y.pow(2)));
-            angle = f32::asin(f32::sin(90.0) * dif_y / c);
-        } else if self.plane.y < _y && _x < self.plane.x {
-            let dif_x = self.plane.x - _x;
-            let dif_y = self.plane.y - _y;
-            let c = f32::sqrt((dif_x.pow(2) + dif_y.pow(2)));
-            angle = f32::asin(f32::sin(90.0) * dif_y / c);
-        }
-        self.plane.rotation = angle;
-        self.plane.flip = _x < self.plane.x;
-    }*/
-
-    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
-        if keycode == KeyCode::Right {
-            self.plane.rotate_right = true;
-        }
-        if keycode == KeyCode::Left {
-            self.plane.rotate_left = true;
-        }
-        if keycode == KeyCode::Space {
-            self.plane.accelerate = true;
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _keycode: KeyCode,
+        _key_mod: KeyMods,
+        _repeat: bool,
+    ) {
+        unsafe {
+            if _keycode == KeyCode::Right {
+                RIGHT_KEY_PRESSED = true;
+            } else if _keycode == KeyCode::Left {
+                LEFT_KEY_PRESSED = true;
+            } else if _keycode == KeyCode::Up {
+                UP_KEY_PRESSED = true;
+            } else if _keycode == KeyCode::Down {
+                DOWN_KEY_PRESSED = true;
+            } else if _keycode == KeyCode::Space {
+                self.plane.boost = true;
+            }
         }
     }
 
     fn key_up_event(&mut self, _ctx: &mut Context, _keycode: KeyCode, _keymods: KeyMods) {
-        if _keycode == KeyCode::Right {
-            self.plane.rotate_right = false;
-        }
-        if _keycode == KeyCode::Left {
-            self.plane.rotate_left = false;
-        }
-        if _keycode == KeyCode::Space {
-            self.plane.accelerate = false;
+        unsafe {
+            if _keycode == KeyCode::F {
+                for bullet in self.bullets.iter_mut() {
+                    if !bullet.active && !bullet.torpedo {
+                        bullet.y = self.plane.y;
+                        bullet.x = self.plane.x;
+                        bullet.active = true;
+                        break;
+                    }
+                }
+            } else if _keycode == KeyCode::Space {
+                for bullet in self.bullets.iter_mut() {
+                    if !bullet.active && bullet.torpedo {
+                        bullet.y = self.plane.y;
+                        bullet.x = self.plane.x;
+                        bullet.active = true;
+                        break;
+                    }
+                }
+            } else if _keycode == KeyCode::Right {
+                RIGHT_KEY_PRESSED = false;
+            } else if _keycode == KeyCode::Left {
+                LEFT_KEY_PRESSED = false;
+            } else if _keycode == KeyCode::Up {
+                UP_KEY_PRESSED = false;
+            } else if _keycode == KeyCode::Down {
+                DOWN_KEY_PRESSED = false;
+            } else if _keycode == KeyCode::Space {
+                self.plane.boost = false;
+            }
         }
     }
 }
-
